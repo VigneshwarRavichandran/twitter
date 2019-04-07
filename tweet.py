6bea282f633a97ef3704ca23e1cbbdc583302101
Vignesh <vigneshwarravichandran@gmail.com>
2019-04-07 12:43:27 +0530

    updated the code with security features
    Signed-off-by: Vignesh <vigneshwarravichandran@gmail.com>

--- tweet.py
+++ tweet.py
@@ -1,20 +1,17 @@
 
 import tweepy
+from security import Secure
 
-access_token = "1372259486-SjppHKngTYOE0yeCVKiIIMP5XnL4RyHqNx92YQV"
-access_token_secret = "1Ee1KKaPtUixMI718ROVGKn26PZbdZTgStfVyVxYwZlVB"
-consumer_key = "q1OjBG5T0DRk6g9mLkOuQfzXJ"
-consumer_secret = "A5vbpHqa6iVmjiCC1JhWeqcPOjpxhGac4mwcO3e5zb6et4RCgc"
 
 def OAuth():
 	try:
-		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-		auth.set_access_token(access_token, access_token_secret)
+		tweet = Secure()
+		auth = tweepy.OAuthHandler(tweet.get_consumer_key(), tweet.get_consumer_secret())
+		auth.set_access_token(tweet.get_access_token(), tweet.get_access_token_secret())
 		return auth
 	except Exception as e:
 		return None
 
 oauth = OAuth()
 api = tweepy.API(oauth)
-
 api.update_status('Hello Tis is Vigneshwar Ravichandran')
